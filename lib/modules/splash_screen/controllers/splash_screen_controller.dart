83934d09d59ede3b7ae6d5f6d221b21ddf76cda8
ratih shinta <rstihshintaaa6@gmail.com>
2023-10-23 22:14:22 +0700

    feat [RS] : membuat page spash screen

--- lib/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -1,8 +1,20 @@
 import 'package:get/get.dart';
 
 class SplashScreenController extends GetxController {
+    Future splashScreen() async {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.offAndToNamed(
+          '/on-boarding-page',
+        );
+      },
+    );
+  }
+  
   @override
   void onInit() {
     super.onInit();
+    splashScreen();
   }
 }
