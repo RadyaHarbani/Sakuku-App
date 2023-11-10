e47997e9dcd42335b39140b7003382560d9ccb64
RadyaHarbani <radya.i4m@gmail.com>
2023-11-11 03:51:29 +0700

    feat [RH] : add profile page component

--- lib/helpers/themes/text_style_themes/profile_page_themes.dart
+++ lib/helpers/themes/text_style_themes/profile_page_themes.dart
@@ -7,12 +7,12 @@ import 'package:sakuku_app/helpers/themes/default_themes.dart';
 TextStyle titleAppbarProfile = GoogleFonts.poppins(
   color: primaryTextColorWhite,
   fontSize: figmaFontsize(17),
-  fontWeight: FontWeight.w500,
+  fontWeight: FontWeight.w600,
 );
 
 TextStyle userNameProfile = GoogleFonts.poppins(
   color: primaryTextColorWhite,
-  fontSize: figmaFontsize(18),
+  fontSize: figmaFontsize(20),
   fontWeight: FontWeight.w600,
 );
 
@@ -20,4 +20,56 @@ TextStyle userEmailProfile = GoogleFonts.poppins(
   color: primaryTextColorWhite,
   fontSize: figmaFontsize(10),
   fontWeight: FontWeight.w400,
+);
+
+TextStyle buttonLogoutProfile = GoogleFonts.poppins(
+  color: primaryTextColorWhite,
+  fontSize: figmaFontsize(16),
+  fontWeight: FontWeight.w600,
+);
+
+//Informasi Pengaturan Component
+TextStyle titleInformasiPengaturan = GoogleFonts.poppins(
+  color: primaryTextColorBlack,
+  fontSize: figmaFontsize(17),
+  fontWeight: FontWeight.bold,
+);
+
+//Khusus Buat Kamu Component
+TextStyle titleKhususBuatKamu = GoogleFonts.poppins(
+  color: primaryTextColorBlack,
+  fontSize: figmaFontsize(17),
+  fontWeight: FontWeight.bold,
+);
+
+//Kontak Bantuan Component
+TextStyle titleKontakBantuan = GoogleFonts.poppins(
+  color: primaryTextColorBlack,
+  fontSize: figmaFontsize(17),
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle subtitleKontakBantuan = GoogleFonts.poppins(
+  color: primaryTextColorBlack,
+  fontSize: figmaFontsize(12),
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle subtitleSosialMedia = GoogleFonts.poppins(
+  color: primaryTextColorBlack,
+  fontSize: figmaFontsize(12),
+  fontWeight: FontWeight.w400,
+);
+
+//Button Text Pengaturan Component
+TextStyle textButtonPengaturan = GoogleFonts.poppins(
+  color: primaryTextColorBlack,
+  fontSize: figmaFontsize(15),
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle chipRekomendasi = GoogleFonts.poppins(
+  color: hintTextColor,
+  fontSize: figmaFontsize(11),
+  fontWeight: FontWeight.w600,
 );
